3889263981d3918085d183ea5a8fe70a78af0435
Onigiri <steinsgate.yn@gmail.com>
2021-01-02 14:15:21 +0800

    feat: add my own git configuration script

---
+++ configs/git/my-configuration.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# !!! WARNING !!!
+# Here is my own configuration, please do not use it.
+git config --global user.name "Onigiri"
+git config --global user.email "steinsgate.yn@gmail.com"
