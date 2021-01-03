c506e7f00096e7fba94dc90a0724d8399536b56c
Onigiri <steinsgate.yn@gmail.com>
2021-01-03 12:10:12 +0800

    feat: add font manager

--- systems/clear-linux/install-dev-apps.sh
+++ systems/clear-linux/install-dev-apps.sh
@@ -2,3 +2,9 @@
 
 # VSCode
 flatpak install flathub com.visualstudio.code
+
+# Font Downloader
+flatpak install flathub org.gustavoperedo.FontDownloader
+
+# Font Manager
+flatpak install flathub org.gnome.FontManager
