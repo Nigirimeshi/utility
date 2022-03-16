514b2fe1b1e7a050216b19faf32c67f3929edadc
Onigiri <steinsgate.yn@gmail.com>
2022-03-16 14:37:20 +0800

    feat: add git config

--- configs/git/my-configuration.sh
+++ configs/git/my-configuration.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 # !!! WARNING !!!
 # Here is my own configuration, please do not use it.
-git config --global user.name "Onigiri"
-git config --global user.email "steinsgate.yn@gmail.com"
+
+git config --global user.name ""
+git config --global user.email ""
+# 提交时转换为 LF，检出时不转换。
+git config --global core.autocrlf input
+# 拒绝提交包含混合换行符的文件。
+git config --global core.safecrlf true
